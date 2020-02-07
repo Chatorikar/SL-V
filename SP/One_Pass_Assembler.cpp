1e6f1a050c35f42258163c843789bff86e2f01eb
Prathamesh J. Chatorikar <51926984+Pratham19Feb@users.noreply.github.com>
2020-02-07 07:31:53 +0530

    Update One_Pass_Assembler.cpp

--- SP/One_Pass_Assembler.cpp
+++ SP/One_Pass_Assembler.cpp
@@ -1,66 +1,76 @@
 #include<bits/stdc++.h>
+
 #include<iostream>
+
 #include<map>
+
 #include<iterator>
+
 using namespace std;
 
-struct symbol_table{
+struct symbol_table {
     int address;
     string symbol;
 };
 
-struct literal_table{
+struct literal_table {
     int address;
     string literal;
 };
-
-class Assembler
-{
+enum e_usflcnst {
+    Aksan = 0,
+        Mandy = 1,
+        Sahriani = 2,
+        start = 4
+};
+class Assembler {
     public:
-        int LOC; 
-        map<string,pair<string,string> >opcode;
-        string Assembly_Program;
-        struct symbol_table s[5];
-        struct literal_table l[5];
-        Assembler();
-        int symbol_count;
-        int literal_count;
-        int Symbol_Table();
-        bool isConstant(string);
-        bool isLiteral(string);
-        bool isSymbol(string);
-        void updateLOC(int,vector<string>);
-        int getAddress(string);
-        void PrintTable();
+        int LOC;
+    map < string, pair < string, string > > opcode;
+    string Assembly_Program;
+    struct symbol_table s[5];
+    struct literal_table l[5];
+    Assembler();
+    int symbol_count;
+    int literal_count;
+    int Symbol_Table();
+    bool isConstant(string);
+    bool isLiteral(string);
+    bool isSymbol(string);
+    void updateLOC(int, vector < string > );
+    int getAddress(string);
+    void PrintTable();
 
-
+    private:
+        bool check_for_start(string);
+        int check_for_type(string);
+        void write_to_file(vector<string>);
 };
 
-Assembler::Assembler()
-{
+Assembler::Assembler() {
 
-    opcode.insert(pair<string,pair<string,string> >("END",make_pair("AD","02")));
-    opcode.insert(pair<string,pair<string,string> >("START",make_pair("AD","01")));
-    opcode.insert(pair<string,pair<string,string> >("ORIGIN",make_pair("AD","03")));
-    opcode.insert(pair<string,pair<string,string> >("EQL",make_pair("AD","04")));
-    opcode.insert(pair<string,pair<string,string> >("LTORG",make_pair("AD","05")));
-    opcode.insert(pair<string,pair<string,string> >("DC",make_pair("DL","01")));
-    opcode.insert(pair<string,pair<string,string> >("DS",make_pair("DL","02")));
-    opcode.insert(pair<string,pair<string,string> >("STOP",make_pair("IS","00")));
-    opcode.insert(pair<string,pair<string,string> >("ADD",make_pair("IS","01")));
-    opcode.insert(pair<string,pair<string,string> >("SUB",make_pair("IS","02")));
-    opcode.insert(pair<string,pair<string,string> >("MULT",make_pair("IS","03")));
-    opcode.insert(pair<string,pair<string,string> >("MOVER",make_pair("IS","04")));
-    opcode.insert(pair<string,pair<string,string> >("MOVEM",make_pair("IS","05")));
-    opcode.insert(pair<string,pair<string,string> >("COMP",make_pair("IS","06")));
-    opcode.insert(pair<string,pair<string,string> >("BC",make_pair("IS","07")));
-    opcode.insert(pair<string,pair<string,string> >("DIV",make_pair("IS","08")));
-    opcode.insert(pair<string,pair<string,string> >("READ",make_pair("IS","09")));
-    opcode.insert(pair<string,pair<string,string> >("PRINT",make_pair("IS","10")));
-    symbol_count  = 0;
+    opcode.insert(pair < string, pair < string, string > > ("END", make_pair("AD", "02")));
+    opcode.insert(pair < string, pair < string, string > > ("START", make_pair("AD", "01")));
+    opcode.insert(pair < string, pair < string, string > > ("ORIGIN", make_pair("AD", "03")));
+    opcode.insert(pair < string, pair < string, string > > ("EQL", make_pair("AD", "04")));
+    opcode.insert(pair < string, pair < string, string > > ("LTORG", make_pair("AD", "05")));
+    opcode.insert(pair < string, pair < string, string > > ("DC", make_pair("DL", "01")));
+    opcode.insert(pair < string, pair < string, string > > ("DS", make_pair("DL", "02")));
+    opcode.insert(pair < string, pair < string, string > > ("STOP", make_pair("IS", "00")));
+    opcode.insert(pair < string, pair < string, string > > ("ADD", make_pair("IS", "01")));
+    opcode.insert(pair < string, pair < string, string > > ("SUB", make_pair("IS", "02")));
+    opcode.insert(pair < string, pair < string, string > > ("MULT", make_pair("IS", "03")));
+    opcode.insert(pair < string, pair < string, string > > ("MOVER", make_pair("IS", "04")));
+    opcode.insert(pair < string, pair < string, string > > ("MOVEM", make_pair("IS", "05")));
+    opcode.insert(pair < string, pair < string, string > > ("COMP", make_pair("IS", "06")));
+    opcode.insert(pair < string, pair < string, string > > ("BC", make_pair("IS", "07")));
+    opcode.insert(pair < string, pair < string, string > > ("DIV", make_pair("IS", "08")));
+    opcode.insert(pair < string, pair < string, string > > ("READ", make_pair("IS", "09")));
+    opcode.insert(pair < string, pair < string, string > > ("PRINT", make_pair("IS", "10")));
+    symbol_count = 0;
     literal_count = 0;
 
-    map<string,pair<string,string> >::iterator it;
+    map < string, pair < string, string > > ::iterator it;
 
     // cout<<" "<<opcode["START"].first;
     // cout<<" "<<opcode["START"].second;
@@ -69,183 +79,264 @@ Assembler::Assembler()
     //     cout<<it->first<<" "<<it->second.first<<" "<<it->second.second<<" "<<endl;
 }
 
-bool Assembler::isLiteral(string s)
-{
-    if(s[0] == '=')
-        return true;
-    return false; 
-}
-
-bool Assembler::isSymbol(string s)
-{
-    // if(s.length() > 0)
-    //     return true;
-    // return false; 
-
-    if(s[0] != '=')
+bool Assembler::isLiteral(string s) {
+    if (s[0] == '=')
         return true;
     return false;
 }
 
+bool Assembler::isSymbol(string s) {
+    if(s.length() == 1)
+        return true;
+    return false; 
 
+}
 
+bool Assembler::isConstant(string s) {
+    for (int i = 0; i < s.length(); i++)
+        if (isdigit(s[i]) == false)
+            return false;
 
-bool Assembler:: isConstant(string s) 
-{ 
-    for (int i = 0; i < s.length(); i++) 
-        if (isdigit(s[i]) == false) 
-            return false; 
-  
-    return true; 
-} 
+    return true;
+}
 
-
-int Assembler::getAddress(string symbol)
-{
-    for(int i = 0 ; i < symbol_count ; i++)
-    {
-        if(s[i].symbol == symbol)
-            return s[i].address; 
+int Assembler::getAddress(string symbol) {
+    for (int i = 0; i < symbol_count; i++) {
+        if (s[i].symbol == symbol)
+            return s[i].address;
     }
 
     return 0;
 }
 
-void Assembler::updateLOC(int LOC , vector<string>v)
-{
-    if(v[1] == "ORIGIN")
-    { 
-       for(int i = 0; i < v[2].length() ; i++)
-        {
-            if(v[i] == "+" || v[i] == "-" )
-            {
-                int address = getAddress(v[2].substr(0,i-1));
-                this->LOC = address + stoi(v[1].substr(i+1,v[2].length()-i));
+void Assembler::updateLOC(int LOC, vector < string > v) {
+    if (v[1] == "ORIGIN") {
+        for (int i = 0; i < v[2].length(); i++) {
+            if (v[i] == "+" || v[i] == "-") {
+                int address = getAddress(v[2].substr(0, i - 1));
+                this -> LOC = address + stoi(v[1].substr(i + 1, v[2].length() - i));
             }
-        } 
-    }
-    else
-        this->LOC++;
+        }
+    } else
+        this -> LOC++;
 }
 
-int Assembler::Symbol_Table()
+
+
+
+bool Assembler::check_for_start(string source_code) {
+    string line , start_word , start_address;
+    vector < string > v;
+    ifstream myfile(source_code);
+    getline(myfile, line);
+    istringstream ss(line);
+    ss >> start_word;
+
+    if (start_word == "START") {
+        ss >> start_address;
+        if (isConstant(start_address))
+        {
+            // if valid start address
+            this -> LOC = LOC = stoi(start_address);
+            cout << "\n\n(" << opcode[start_word].first << " , " << opcode[start_word].second << ")" << " (C ," << start_address << ")" << endl;
+
+        }
+        else
+            return false;
+
+        return true;
+    }
+    return false;
+}
+
+
+int Assembler::check_for_type(string str)
 {
+
+    if(isLiteral(str))
+        return 1;
+    
+    if(isConstant(str))
+        return 3;
+    
+    if(isSymbol(str))
+        return 2;
+}
+
+int Assembler::Symbol_Table() {
     // Program File Name :
-    cout<<endl<<"Enter Assembly Program File Name :";
+    cout << endl << "Enter Assembly Program File Name :";
     // cin>>Assembly_Program;
 
     //Open a Souce Code file
-    string line;
-    vector<string> v;
-    vector<string>update_loc;
-    ifstream myfile ("input.txt");
-    while (getline(myfile, line))
-    {
-        istringstream ss(line); 
+    string line, source_code("input.txt");
+    vector < string > v;
+    bool flag = true;
+    vector < string > update_loc;
 
-        do { 
-            string word; 
-            ss >> word;
-            v.push_back(word); 
-    
-        } while (ss); 
+    // cout<<"\nEnter Souce Code file name :  ";
+    // cin>>source_code;
 
-        if(v[1] == "START")
+    if (!check_for_start(source_code)) {
+        cout << "Error: Invalid Start ";
+        return 0;
+    }
+
+    ifstream myfile(source_code);
+        
+
+       
+           
+            
+    while (getline(myfile, line)) {
+
+        if(flag)
         {
-            //where str is your number as std::string.
-            LOC = stoi(v[2]); 
-            if(isConstant(v[2]))
-                cout<<endl<<"("<<opcode[v[1]].first<<" , "<<opcode[v[1]].second<<")"<<" (C ,"<<LOC<<")"<<endl;            
-            else{
-                cout<<"Error: Invalid Start Address ";
-                return 0;
-            }
+            // to skip first line from souce code as we already process in check_for_start() method
+            flag = false;
+            continue;
         }
 
-     else if(v[0]=="-")
-       {
-            if(v[1] == "END")
-            {
-                cout<<"("<<opcode[v[1]].first<<" , "<<opcode[v[1]].second<<")"<<" "<<LOC<<endl;
-                updateLOC(LOC,v);
+        istringstream ss(line);
 
-            }    
-            else if(isSymbol(v[3])) // check
-            {   
+        do {
+            string word;
+            ss >> word;
+            v.push_back(word);
 
-                cout<<"("<<opcode[v[1]].first<<" , "<<opcode[v[1]].second<<")"<<" (S ,"<<symbol_count<<")"<<" "<<LOC<<endl;
-                s[symbol_count].symbol = v[3];
-                symbol_count++;            
-                updateLOC(LOC,v);
-            }
-            else if(isLiteral(v[3]))
-            {
-                cout<<"("<<opcode[v[1]].first<<" , "<<opcode[v[1]].second<<")"<<" (L ,"<<literal_count<<")"<<" "<<LOC<<endl;
-                l[literal_count].literal = v[3];
-                literal_count++; 
-                updateLOC(LOC,v);
-            }
-       }    
-       else if(isSymbol(v[0]))
-       {
-            if(v[1] == "END")
-            {
-                cout<<"("<<opcode[v[1]].first<<" , "<<opcode[v[1]].second<<")"<<" "<<LOC<<endl;
+        } while (ss);
 
-            }    
-            else if(isSymbol(v[3]))
-            {   
-                cout<<"("<<v[0]<<" , "<<symbol_count<<")"<<"("<<opcode[v[1]].first<<" , "<<opcode[v[1]].second<<")"<<" (S ,"<<symbol_count<<")"<<" "<<LOC<<endl;                
-                s[symbol_count].symbol = v[0];
-                s[symbol_count].address = LOC;
-                symbol_count++;            
-                
-                updateLOC(LOC,v);
-                s[symbol_count].symbol = v[3];
-                
-            }
-            else if(isLiteral(v[3]))
-            {
-                cout<<"("<<v[0]<<" , "<<symbol_count<<"("<<opcode[v[0]].first<<" , "<<opcode[v[0]].second<<")"<<"("<<"("<<opcode[v[1]].first<<" , "<<opcode[v[1]].second<<")"<<" (L ,"<<literal_count<<")"<<" "<<LOC<<endl;
-                l[literal_count].literal = v[3];
-                literal_count++; 
-
-                s[symbol_count].symbol = v[0];
-                s[symbol_count].address = LOC;
-                symbol_count++;            
-                
-                updateLOC(LOC,v);
+        if(opcode.count(v[0]) > 0)
+        {   
+            //opcode.count will give true or false if we have corresonding entry in OPCODE map or not
+            //if MOVEM AREG B i.e. V[0] we have IS 
+            // v[0] v[1] v[2]
             
-            }            
-       }
+            switch(check_for_type(v[2])) // check for start as it is having only two value v[0] v[1]
+            {
+                case 1:
+                    // if it is literal
+                    cout << "(" << opcode[v[0]].first << " , " << opcode[v[0]].second << ")" << " (L ," << this->literal_count << ")" << " " <<this->LOC << endl;
+                    l[literal_count].literal = v[2];
+                    literal_count++;
+                    break;
+                case 2:
+                    // if it is symbol
+                    cout << "(" << opcode[v[0]].first << " , " << opcode[v[0]].second << ")" << " (S ," << symbol_count << ")" << " " << this->LOC << endl;
+                     // for processsing a lable but we have to print it's value before incrementing counter , also we have to increment counter to allocate for next symb
+                    s[symbol_count].symbol = v[2];
+                    symbol_count++;
+                    break;
+
+                case 3:
+                    // if it is constant 
+                    cout << "(" << opcode[v[0]].first << " , " << opcode[v[0]].second << ")" << " (C ," << v[2] << ")" << " " << this->LOC << endl;
+                    break;
+            }
+            
+            updateLOC(LOC, v);
+
+        }else {
+
+            // v[0] i.e. label in symbol table and add address to label
+            s[symbol_count].symbol = v[0]; 
+            s[symbol_count].address = this->LOC;
+                    symbol_count++;
+
+            switch(check_for_type(v[3])) 
+            {
+                // if *Lable* MOVEM AREG B i.e. V[0] we have Label put it into ST
+                case 1:
+                    // if it is literal
+                    cout <<"(" <<v[0] <<" , "<<symbol_count-1<< ")(" << opcode[v[1]].first << " , " << opcode[v[1]].second << ")" << " (L ," << literal_count << ")" << " " <<this->LOC << endl;
+                    l[literal_count].literal = v[3];
+                    literal_count++;
+                    break;
+                case 2:
+                    // if it is symbol
+                    cout <<"(" <<v[0] <<" , "<<symbol_count-1<< ")(" << opcode[v[1]].first << " , " << opcode[v[1]].second << ")" << " (S ," << symbol_count << ")" << " " <<this->LOC << endl;
+                    s[symbol_count].symbol = v[3];
+                    symbol_count++;
+                    break;
+
+                case 3:
+                    // if it is constant 
+                    cout <<"(" <<v[0] <<" , "<<symbol_count<< ")(" << opcode[v[1]].first << " , " << opcode[v[1]].second << ")" << " (L ," << v[3] << ")" << " " <<this->LOC << endl;
+                    break;
+            }
+
+           
+            updateLOC(LOC, v);
+        }
+
+
+        // if (v[0] == "-") {
+        //     if (v[1] == "END") {
+        //         cout << "(" << opcode[v[1]].first << " , " << opcode[v[1]].second << ")" << " " << LOC << endl;
+        //         updateLOC(LOC, v);
+
+        //     } else if (isSymbol(v[3])) // check
+        //     {
+
+        //         cout << "(" << opcode[v[1]].first << " , " << opcode[v[1]].second << ")" << " (S ," << symbol_count << ")" << " " << LOC << endl;
+        //         s[symbol_count].symbol = v[3];
+        //         symbol_count++;
+        //         updateLOC(LOC, v);
+        //     } else if (isLiteral(v[3])) {
+        //         cout << "(" << opcode[v[1]].first << " , " << opcode[v[1]].second << ")" << " (L ," << literal_count << ")" << " " << LOC << endl;
+        //         l[literal_count].literal = v[3];
+        //         literal_count++;
+        //         updateLOC(LOC, v);
+        //     }
+        // } else if (isSymbol(v[0])) {
+        //     if (v[1] == "END") {
+        //         cout << "(" << opcode[v[1]].first << " , " << opcode[v[1]].second << ")" << " " << LOC << endl;
+
+        //     } else if (isSymbol(v[3])) {
+        //         cout << "(" << v[0] << " , " << symbol_count << ")" << "(" << opcode[v[1]].first << " , " << opcode[v[1]].second << ")" << " (S ," << symbol_count << ")" << " " << LOC << endl;
+        //         s[symbol_count].symbol = v[0];
+        //         s[symbol_count].address = LOC;
+        //         symbol_count++;
+
+        //         updateLOC(LOC, v);
+        //         s[symbol_count].symbol = v[3];
+
+        //     } else if (isLiteral(v[3])) {
+        //         cout << "(" << v[0] << " , " << symbol_count << "(" << opcode[v[0]].first << " , " << opcode[v[0]].second << ")" << "(" << "(" << opcode[v[1]].first << " , " << opcode[v[1]].second << ")" << " (L ," << literal_count << ")" << " " << LOC << endl;
+        //         l[literal_count].literal = v[3];
+        //         literal_count++;
+
+        //         s[symbol_count].symbol = v[0];
+        //         s[symbol_count].address = LOC;
+        //         symbol_count++;
+
+        //         updateLOC(LOC, v);
+
+        //     }
+        // }
 
         v.clear();
     }
 
-   return 1;
+    return 1;
 }
 
-void Assembler::PrintTable()
-{
-    for(int i = 0 ; i < symbol_count ; i++)
-    {
-        cout<<s[i].symbol<<"  "<<s[i].address<<endl;
+void Assembler::PrintTable() {
+    for (int i = 0; i < symbol_count; i++) {
+        cout << s[i].symbol << "  " << s[i].address << endl;
     }
 
-    cout<<endl<<endl<<endl;
+    cout << endl << endl << endl;
 
-
-    for(int i = 0 ; i < literal_count ; i++)
-    {
-        cout<<l[i].literal<<"  "<<l[i].address<<endl;
+    for (int i = 0; i < literal_count; i++) {
+        cout << l[i].literal << "  " << l[i].address << endl;
     }
 }
 
-int main()
-{
+int main() {
 
-    Assembler *Obj = new Assembler;
-    Obj->Symbol_Table();
-    Obj->PrintTable();
+    Assembler * Obj = new Assembler;
+    Obj -> Symbol_Table();
+ //   Obj -> PrintTable();
 
 }
